39498c603464685eb2bbf93787f74695daddf07b
John Heng <chronorus@gmail.com>
2020-05-30 16:37:31 +0700

    test

---
+++ sandbox1.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+parseString = require('xml2js').parseString;
+
+// let xml_string = fs.readFileSync("ETDA-invoice.xml", "utf8");
+fs.readFileSync("ETDA-invoice.xml", "utf8");
+
